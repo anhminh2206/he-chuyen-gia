22079fa6cdf0c9eac54f7e14ba90e0d66af1485e
anhminh2206 <91835952+anhminh2206@users.noreply.github.com>
2023-03-02 15:09:11 +0700

    Add files via upload

---
+++ Hệ Chuyên Gia Tư Vấn Ngành học (1)/HeChuyenGia_Nhom2/HeChuyenGia_Nhom2/HeChuyenGia_Nhom16/Main.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace HeChuyenGia_Nhom2
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsukien = new System.Windows.Forms.Button();
+            this.btnluat = new System.Windows.Forms.Button();
+            this.btntuvan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnsukien
+            // 
+            this.btnsukien.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnsukien.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnsukien.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnsukien.Location = new System.Drawing.Point(35, 108);
+            this.btnsukien.Name = "btnsukien";
+            this.btnsukien.Size = new System.Drawing.Size(134, 39);
+            this.btnsukien.TabIndex = 0;
+            this.btnsukien.Text = "Sự Kiện";
+            this.btnsukien.UseVisualStyleBackColor = false;
+            this.btnsukien.Click += new System.EventHandler(this.btnsukien_Click);
+            // 
+            // btnluat
+            // 
+            this.btnluat.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnluat.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnluat.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnluat.Location = new System.Drawing.Point(35, 153);
+            this.btnluat.Name = "btnluat";
+            this.btnluat.Size = new System.Drawing.Size(134, 39);
+            this.btnluat.TabIndex = 0;
+            this.btnluat.Text = "Luật";
+            this.btnluat.UseVisualStyleBackColor = false;
+            this.btnluat.Click += new System.EventHandler(this.btnluat_Click);
+            // 
+            // btntuvan
+            // 
+            this.btntuvan.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btntuvan.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btntuvan.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btntuvan.Location = new System.Drawing.Point(35, 261);
+            this.btntuvan.Name = "btntuvan";
+            this.btntuvan.Size = new System.Drawing.Size(134, 39);
+            this.btntuvan.TabIndex = 0;
+            this.btntuvan.Text = "Tư Vấn";
+            this.btntuvan.UseVisualStyleBackColor = false;
+            this.btntuvan.Click += new System.EventHandler(this.btntuvan_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::HeChuyenGia_Nhom2.Properties.Resources.chon_nghe_phu_hop;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(891, 531);
+            this.Controls.Add(this.btntuvan);
+            this.Controls.Add(this.btnluat);
+            this.Controls.Add(this.btnsukien);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Main";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "HỆ CHUYÊN GIA TƯ VẤN CHỌN NGÀNH HỌC";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnsukien;
+        private System.Windows.Forms.Button btnluat;
+        private System.Windows.Forms.Button btntuvan;
+    }
+}
+
---
+++ Hệ Chuyên Gia Tư Vấn Ngành học (1)/HeChuyenGia_Nhom2/HeChuyenGia_Nhom2/HeChuyenGia_Nhom16/tuvan.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace HeChuyenGia_Nhom2
+{
+    partial class tuvan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.ricKQ = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnTuVan = new System.Windows.Forms.Button();
+            this.cbsothic = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbnhomnganh = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbkhoi = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbhocluc = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(822, 452);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(402, 32);
+            this.progressBar.TabIndex = 26;
+            // 
+            // ricKQ
+            // 
+            this.ricKQ.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ricKQ.Enabled = false;
+            this.ricKQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ricKQ.Location = new System.Drawing.Point(701, 494);
+            this.ricKQ.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.ricKQ.Name = "ricKQ";
+            this.ricKQ.ReadOnly = true;
+            this.ricKQ.Size = new System.Drawing.Size(523, 142);
+            this.ricKQ.TabIndex = 25;
+            this.ricKQ.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(695, 452);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(117, 35);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "KẾT QUẢ";
+            // 
+            // btnTuVan
+            // 
+            this.btnTuVan.BackgroundImage = global::HeChuyenGia_Nhom2.Properties.Resources.tu_van_chon_nghe;
+            this.btnTuVan.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTuVan.Location = new System.Drawing.Point(701, 351);
+            this.btnTuVan.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnTuVan.Name = "btnTuVan";
+            this.btnTuVan.Size = new System.Drawing.Size(201, 60);
+            this.btnTuVan.TabIndex = 22;
+            this.btnTuVan.Text = "TƯ VẤN";
+            this.btnTuVan.UseVisualStyleBackColor = true;
+            this.btnTuVan.Click += new System.EventHandler(this.btnTuVan_Click);
+            // 
+            // cbsothic
+            // 
+            this.cbsothic.BackColor = System.Drawing.SystemColors.Window;
+            this.cbsothic.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbsothic.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.cbsothic.FormattingEnabled = true;
+            this.cbsothic.Location = new System.Drawing.Point(871, 270);
+            this.cbsothic.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.cbsothic.Name = "cbsothic";
+            this.cbsothic.Size = new System.Drawing.Size(353, 26);
+            this.cbsothic.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(697, 273);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 24);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Sở thích";
+            // 
+            // cbnhomnganh
+            // 
+            this.cbnhomnganh.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbnhomnganh.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.cbnhomnganh.FormattingEnabled = true;
+            this.cbnhomnganh.Location = new System.Drawing.Point(871, 213);
+            this.cbnhomnganh.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.cbnhomnganh.Name = "cbnhomnganh";
+            this.cbnhomnganh.Size = new System.Drawing.Size(353, 26);
+            this.cbnhomnganh.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(697, 213);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 24);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Nhóm Ngành";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // cbkhoi
+            // 
+            this.cbkhoi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbkhoi.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.cbkhoi.FormattingEnabled = true;
+            this.cbkhoi.Location = new System.Drawing.Point(871, 97);
+            this.cbkhoi.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.cbkhoi.Name = "cbkhoi";
+            this.cbkhoi.Size = new System.Drawing.Size(353, 26);
+            this.cbkhoi.TabIndex = 17;
+            this.cbkhoi.SelectedIndexChanged += new System.EventHandler(this.cbkhoi_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(697, 100);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 24);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Khối thi";
+            // 
+            // cbhocluc
+            // 
+            this.cbhocluc.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbhocluc.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.cbhocluc.FormattingEnabled = true;
+            this.cbhocluc.Location = new System.Drawing.Point(871, 153);
+            this.cbhocluc.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.cbhocluc.Name = "cbhocluc";
+            this.cbhocluc.Size = new System.Drawing.Size(353, 26);
+            this.cbhocluc.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(697, 156);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 24);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Học lực của bạn";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackgroundImage = global::HeChuyenGia_Nhom2.Properties.Resources.tu_van_chon_nghe;
+            this.btnReset.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(1096, 351);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(128, 60);
+            this.btnReset.TabIndex = 27;
+            this.btnReset.Text = "HỦY";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::HeChuyenGia_Nhom2.Properties.Resources.tv;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(12, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(647, 637);
+            this.panel1.TabIndex = 28;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(752, 24);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(450, 29);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "HỆ CHUYÊN GIA TƯ VẤN CHỌN NGÀNH HỌC";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // tuvan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1273, 647);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.ricKQ);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnTuVan);
+            this.Controls.Add(this.cbsothic);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbnhomnganh);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbkhoi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbhocluc);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "tuvan";
+            this.Text = "tuvan";
+            this.Load += new System.EventHandler(this.tuvan_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.RichTextBox ricKQ;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnTuVan;
+        private System.Windows.Forms.ComboBox cbsothic;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbnhomnganh;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbkhoi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbhocluc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+    }
+}
